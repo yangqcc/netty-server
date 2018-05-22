2e840044675146ad1126c836190a403f7acb6e76
yangqicheng <yangqicheng@daianla.cn>
2018-05-22 17:55:28 +0800

    Initial commit

---
+++ client/src/main/java/com/yqc/client/EchoClientHandler.java
@@ -0,0 +1,36 @@
+package com.yqc.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 客户端handler
+ *
+ * @author yangqc
+ */
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        //当被通知该 channel 是活动的时候就发送信息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
+        //记录接收到的消息
+        System.out.println("Client received: " + in.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        //记录日志错误并关闭 channel
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
