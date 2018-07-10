e710b8e7d918ef094c674fded34a93df2dc5b343
yangqc <yangqc_ars@outlook.com>
2018-07-10 22:48:07 +0800

    nio测试

---
+++ nio/src/main/java/com/yqc/netty/PlainOioServer.java
@@ -0,0 +1,59 @@
+package com.yqc.netty;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * <p>title:</p>
+ * <p>description:</p>
+ *
+ * @author yangqc
+ * @date Created in 2018-06-03
+ * @modified By yangqc
+ */
+public class PlainOioServer {
+
+  /**
+   * 1.绑定服务器到指定的端口。
+   *
+   * 2.接受一个连接。
+   *
+   * 3.创建一个新的线程来处理连接。
+   *
+   * 4.将消息发送到连接的客户端。
+   *
+   * 5.一旦消息被写入和刷新时就 关闭连接。
+   *
+   * 6.启动线程。
+   */
+  public void serve(int port) throws IOException {
+    final ServerSocket socket = new ServerSocket(port);
+    try {
+      for (; ; ) {
+        final Socket clientSocket = socket.accept();
+        System.out.println("Accepted connection from" + clientSocket);
+        new Thread(() -> {
+          OutputStream out;
+          try {
+            out = clientSocket.getOutputStream();
+            out.write("Hi!\r\n".getBytes(Charset.forName("UTF-8")));
+            out.flush();
+            clientSocket.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+            try {
+              clientSocket.close();
+            } catch (IOException ex) {
+              ex.printStackTrace();
+            }
+          }
+        }).start();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
