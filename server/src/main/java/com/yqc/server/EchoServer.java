f1a0f8c2c6ae64b56f7b36ccdc73b5d19762a185
yangqc <yangqc_ars@outlook.com>
2018-10-12 22:42:02 +0800

    格式化文件

--- server/src/main/java/com/yqc/server/EchoServer.java
+++ server/src/main/java/com/yqc/server/EchoServer.java
@@ -6,47 +6,56 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-
 import java.net.InetSocketAddress;
 
+/**
+ * @author yangqc
+ */
 public class EchoServer {
-    private final int port;
 
-    public EchoServer(int port) {
-        this.port = port;
+  private final int port;
+
+  public EchoServer(int port) {
+    this.port = port;
+  }
+
+  public void start() throws Exception {
+    //创建 EventLoopGroup
+    NioEventLoopGroup group = new NioEventLoopGroup();
+    try {
+      ServerBootstrap b = new ServerBootstrap();
+      //创建 ServerBootstrap
+      b.group(group)
+          //指定使用 NIO 的传输 Channel
+          .channel(NioServerSocketChannel.class)
+          //设置 socket 地址使用所选的端口
+          .localAddress(new InetSocketAddress(port))
+          //添加 EchoServerHandler 到 Channel 的 ChannelPipeline
+          .childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) throws Exception {
+              ch.pipeline().addLast(new EchoServerHandler());
+            }
+          });
+
+      //绑定的服务器;sync 等待服务器关闭
+      ChannelFuture f = b.bind().sync();
+      System.out.println(
+          EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
+      f.channel().closeFuture().sync();//关闭 channel 和 块，直到它被关闭
+    } finally {
+      group.shutdownGracefully().sync();//关机的 EventLoopGroup，释放所有资源。
     }
+  }
 
-    public void start() throws Exception {
-        //创建 EventLoopGroup
-        NioEventLoopGroup group = new NioEventLoopGroup();
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(group) //创建 ServerBootstrap
-                    .channel(NioServerSocketChannel.class)        //指定使用 NIO 的传输 Channel
-                    .localAddress(new InetSocketAddress(port))    //设置 socket 地址使用所选的端口
-                    .childHandler(new ChannelInitializer<SocketChannel>() { //添加 EchoServerHandler 到 Channel 的 ChannelPipeline
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new EchoServerHandler());
-                        }
-                    });
-
-            ChannelFuture f = b.bind().sync();//绑定的服务器;sync 等待服务器关闭
-            System.out.println(EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
-            f.channel().closeFuture().sync();//关闭 channel 和 块，直到它被关闭
-        } finally {
-            group.shutdownGracefully().sync();//关机的 EventLoopGroup，释放所有资源。
-        }
+  public static void main(String[] args) throws Exception {
+    if (args.length != 1) {
+      System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
+      return;
     }
-
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
-            return;
-        }
-        //设置端口值（抛出一个 NumberFormatException 如果该端口参数的格式不正确）
-        int port = Integer.parseInt(args[0]);
-        //调用服务器的 start() 方法
-        new EchoServer(port).start();
-    }
+    //设置端口值（抛出一个 NumberFormatException 如果该端口参数的格式不正确）
+    int port = Integer.parseInt(args[0]);
+    //调用服务器的 start() 方法
+    new EchoServer(port).start();
+  }
 }
